899cd774ddbcbcde27df65c99fc5cdefabda5a58
devrakibulrahman <devrakibulrahman@gmail.com>
2024-12-28 20:37:42 +0600

    update the router jsx file and added the auth router done on 12/28/2024 at 08:37 pm

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from '../App';
 import Home from "../pages/Home";
+import Auth from "../pages/Auth";
+import SignUpPage from "../pages/Auth/SignUpPage";
+import SignInPage from "../pages/Auth/SignInPage";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +13,20 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
+            },
+            {
+                path: '/auth',
+                element: <Auth></Auth>,
+                children: [
+                    {
+                        path: '/auth/sign_up',
+                        element: <SignUpPage></SignUpPage>
+                    },
+                    {
+                        path: '/auth/sign_in',
+                        element: <SignInPage></SignInPage>
+                    },
+                ],
             },
         ],
     },
