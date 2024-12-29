b18bf85560dab636ab17cf2829df622f56bca3bc
devrakibulrahman <devrakibulrahman@gmail.com>
2024-12-30 01:04:53 +0600

    added gap between the contact card component done on 12/30/2024 at 01:04 am

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -84,7 +84,7 @@ const Contact = () => {
                             </div>
                         </div>
                         <div className="w-full mt-16 lg:mt-24">
-                            <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                            <div className="w-full grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
                                 {
                                     contactInfo?.map((data) => (
                                         <ContactCard key={data?.id} contact={data}></ContactCard>
