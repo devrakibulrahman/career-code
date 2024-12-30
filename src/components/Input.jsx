8158e8cc1c452dd1cd27361532cdfbc87b4c62ec
devrakibulrahman <devrakibulrahman@gmail.com>
2024-12-31 01:07:11 +0600

    update in the input component change text-[14px] to text-10 done on 12/31/2024 at 01:07 am

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,7 @@ const Input = ({type, name, placeholder, defaultValue, setValues}) => {
 
     return (
         <>
-            <input type={type} name={name} placeholder={placeholder} defaultValue={defaultValue} onChange={(e) => setValues(e.target.value)} className="font-jakarta font-normal text-[14px] text-gray-800 border border-slate-200/50 w-full h-[40px] px-3 py-2 rounded-md placeholder:text-gray-500 focus:border-emerald-600 outline-none transition-all ease-linear duration-200" />
+            <input type={type} name={name} placeholder={placeholder} defaultValue={defaultValue} onChange={(e) => setValues(e.target.value)} className="font-jakarta font-normal text-sm text-gray-800 border border-slate-200/50 w-full h-[40px] px-3 py-2 rounded-md placeholder:text-gray-500 focus:border-emerald-600 outline-none transition-all ease-linear duration-200" />
         </>
     );
 };
